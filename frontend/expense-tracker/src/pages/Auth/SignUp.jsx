17d9ac866982b9b9ef12f03d932191823b95896e
muskanqed <muskanqed@gmail.com>
2025-08-06 08:01:47 -0400

    Started with signup page

--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import AuthLayout from '../../components/layouts/AuthLayout';
+import Input from '../../components/Inputs/Input';
+import { Link, useNavigate } from 'react-router-dom';
+import { validateEmail } from '../../utils/helper';
 
 const SignUp = () => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Handle signup form submit
+  const handleSignUp = async (e) => {
+
+  }
+
   return (
-    <div>
-      SignUp
-    </div>
+    <AuthLayout>
+      <div className='lg:w-[100%] h-auto md:h-full mt-10 flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Create an Account</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+          Join us today by entering your details below
+        </p>
+
+        <form onSubmit={handleSignUp}>
+          <div>
+
+          </div>
+
+        </form>
+      </div>
+    </AuthLayout>
   )
 }
 
