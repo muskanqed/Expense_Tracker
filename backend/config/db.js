e89196e5c592b2d78e0381820d7ac8f58d75ae20
muskanqed <muskanqed@gmail.com>
2025-08-09 14:32:14 -0400

    Done with register route

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL, {})
+        await mongoose.connect(process.env.MONGODB_URL, {});
         console.log("MongoDB connected");
-    }
-    catch (err) {
+    } catch (err) {
         console.log("Error in connecting to DB", err);
         process.exit(1);
     }
-}
+};
 
 module.exports = connectDB;
