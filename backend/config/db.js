fe5e7acb2ffaffeb012152cedab4472ef4c0f45d
muskanqed <muskanqed@gmail.com>
2025-08-09 06:30:58 -0400

    Added Database connection

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {})
+        console.log("MongoDB connected");
+    }
+    catch (err) {
+        console.log("Error in connecting to DB", err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -10,16 +11,16 @@ app.use(
     cors({
         origin: process.env.CLIENT_URL || "*",
         methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"]
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
-)
+);
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000
+connectDB();
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
-
-})
-
+});
