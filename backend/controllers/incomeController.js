5b4687d5bf507cf47b84054be429588a6ef65087
muskanqed <muskanqed@gmail.com>
2025-08-12 03:35:18 -0400

    Added controller for adding income

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,21 +1,37 @@
-
+const User = require("../models/User");
+const Income = require("../models/Income");
 
 // Add Income Source
 exports.addIncome = async (req, res) => {
+    const userId = req.user.id;
+    try {
+        const { icon, source, amount, date } = req.body || {};
 
-}
+        // Validation check for missing fields
+        if (!source || !amount || !date) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const newIncome = new Income({
+            userId,
+            icon,
+            source,
+            amount,
+            date: new Date(date),
+        });
+
+        await newIncome.save();
+        res.status(200).json(newIncome);
+    } catch (err) {
+        res.status(500).json({ message: "Server Error", Error: err.message });
+    }
+};
 
 // Get all source income
-exports.getAllIncome = async (req, res) => {
-
-}
+exports.getAllIncome = async (req, res) => { };
 
 // Download Excel
-exports.downloadLoadExcel = async (req, res) => {
-
-}
+exports.downloadLoadExcel = async (req, res) => { };
 
 // Delete the source income
-exports.deleteIncome = async (req, res) => {
-
-}
+exports.deleteIncome = async (req, res) => { };
