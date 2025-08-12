f320ab476b85869ac29012faedbf7560e4c21e76
muskanqed <muskanqed@gmail.com>
2025-08-12 04:09:21 -0400

    Added get income controller

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -28,7 +28,16 @@ exports.addIncome = async (req, res) => {
 };
 
 // Get all source income
-exports.getAllIncome = async (req, res) => { };
+exports.getAllIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const income = await Income.find({ userId }).sort({ date: -1 });
+        res.json(income);
+    } catch (err) {
+        res.status(500).json({ message: "Server Error", Error: err.message });
+    }
+};
 
 // Download Excel
 exports.downloadLoadExcel = async (req, res) => { };
