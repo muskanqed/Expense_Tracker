0cf3b660072b6cccdd44d92de9992c32eb032333
muskanqed <muskanqed@gmail.com>
2025-09-28 05:29:05 -0400

    Restructure and Feature addition

---
+++ frontend/src/utils/apiPath.js
@@ -0,0 +1,27 @@
+import Income from "../../../../backend/models/Income"
+
+export const BASE_URL = "http://localhost:8000"
+
+// utils/apiPath.js
+export const API_PATH = {
+    AUTH: {
+        LOGIN: "/api/v1/auth/login",
+        REGISTER: "/api/v1/auth/register",
+        GET_USER_INFO: "/api/v1/auth/getUser",
+    },
+    INCOME: {
+        ADD_INCOME: "/api/v1/income/add",
+        GET_ALL_INCOME: "/api/v1/income/get",
+        DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
+        DOWNLOAD_INCOME: `/api/v1/income/downloadLoadExcel`,
+    },
+    DASHBOARD: {
+        GET_DATA: "/api/v1/dashboard"
+    },
+    EXPENSE: {
+        ADD_INCOME: "/api/v1/income/add",
+        GET_ALL_INCOME: "/api/v1/income/get",
+        DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
+        DOWNLOAD_INCOME: `/api/v1/income/downloadLoadExcel`,
+    },
+}
