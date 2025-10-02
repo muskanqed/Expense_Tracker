f9cd2771218a66522738397264395a4d26be8fb8
muskanqed <muskanqed@gmail.com>
2025-10-02 03:00:49 -0400

    feat: Information of expense,income and total to display on user dashboard

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -15,3 +15,15 @@ export const getInitials = (name) => {
     }
     return initials.toUpperCase();
 }
+
+export const addThousandsSeparator = (num) => {
+    if (num == null || isNaN(num)) return "";
+
+    const [integerParts, fractionalPart] = num.toString().split(".");
+    const formattedInteger = integerParts.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    return fractionalPart
+        ? `${formattedInteger}.${fractionalPart}`
+        : formattedInteger
+
+}
