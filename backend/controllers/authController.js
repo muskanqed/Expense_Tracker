048ce21adf3369c750f1be603004f1a8c4346b8a
muskanqed <muskanqed@gmail.com>
2025-10-01 23:14:44 -0400

    fix(auth): secure getUserInfo with JWT middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -70,7 +70,7 @@ exports.loginUser = async (req, res) => {
 
 exports.getUserInfo = async (req, res) => {
     try {
-        const user = await User.findById(req.body._id).select("-password");
+        const user = await User.findById(req.user._id).select("-password");
 
         if (!user) {
             return res.status(404).json({ message: "User not found" });
